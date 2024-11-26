7513cc196829b041765a202baf835fed738c4530
Ali Belkhair <alibelkhaircontact@gmai.com>
2024-11-26 22:42:22 +0100

    Mini_CMS -version0.1

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -13,7 +13,8 @@ class PostController extends Controller
      */
     public function index()
     {
-        $posts = Post::with('user')->latest()->get();
+        // Fetch posts for the logged-in user
+        $posts = Post::where('user_id', auth()->id())->get();
         return view('posts.index', compact('posts'));
     }
 
